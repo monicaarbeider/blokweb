9cdfd5c4b3346b5408a3510e5b1653d9798327bd
monicaarbeider <43567456+monicaarbeider@users.noreply.github.com>
2021-01-10 04:11:20 +0100

    Overschrijven mislukt, nu wel?

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+
+document.getElementById("allerhande-menu-toggle").addEventListener("click", function() {
+  document.getElementById("toggle-menu").classList.toggle('toggled');
+});
+
+document.getElementById("overlay-toggle").addEventListener("click", function() {
+  document.getElementById("toggle-menu").classList.toggle('toggled');
+});
